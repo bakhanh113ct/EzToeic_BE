d02137bfe7628d2ed6241d82f591ff0708db6329
bakhanh113ct <84719735+bakhanh113ct@users.noreply.github.com>
2023-12-12 00:13:40 +0700

    Update ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -7,7 +7,7 @@ module.exports = {
                 PORT:8000,
                 ACCESS_TOKEN_SECRET:'khanh',
                 REFRESH_TOKEN_SECRET:'refresh',
-                REDIS_URL:'rediss://red-clllogb8772c73fdao00:Uwmv4z9BpXI54fqtuVJGJJPOIW4ThZ8o@singapore-redis.render.com:6379',
+                REDIS_URL:'rediss://red-clrk2vnqd2ns739sj4a0:UAthZmuoF0wFMbZGP6ZhWyel29Y3M0GF@singapore-redis.render.com:6379',
                 DATABASE_HOST:'dpg-ckju62pjrl0c73fq6pj0-a.singapore-postgres.render.com',
                 DATABASE_PORT:5432,
                 DATABASE_USERNAME:'eztoeic_user',
